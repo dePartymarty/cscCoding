9a5bbab64e8e763435c9f7d12d2266fc7d5a0ac2
dePartymarty <martin.spinell@cuw.edu>
2021-03-02 00:49:11 -0600

    started new repository

---
+++ LinkedList.hpp
@@ -0,0 +1,17 @@
+#include "Node.hpp"
+
+class LinkedList
+{
+    private:
+        Node* head;
+        Node* tail;
+        int count;
+
+    public:
+        LinkedList();
+        void display();
+        void addEnd(int value);
+        void addFront(int value);
+        int removeFront();
+        int removeEnd();
+};
